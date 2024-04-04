ce08e0656831174a02cbf688f89e3ffc0271966e
hemendrasahu67 <57944144+hemendrasahu67@users.noreply.github.com>
2024-04-04 18:57:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-API_ID = API_ID = 25919081
+API_ID = API_ID = 28528007
 
-API_HASH = os.environ.get("API_HASH", "0bc2fdba14b16b44f0d89729ed8d2118")
+API_HASH = os.environ.get("API_HASH", "38464da16c80310cabc8d13952419cf3")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6911744817:AAFH-5aOTr9hrR-VYmHFKK4w_1nXsbQ-vqw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7031030184:AAHmGa2u2CR6hQgDrHxbUcKeaIkw_qrWlHA")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 1826828317))
+OWNER = int(os.environ.get("OWNER", 6717769514))
 
-LOG = -1002015954185
+LOG = -1002095973382
 
